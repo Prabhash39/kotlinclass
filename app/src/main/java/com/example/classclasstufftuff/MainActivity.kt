e428cf7544b0422fd802745b296cd86443409ff6
Prabhash39 <prabhashsigdel10@gmail.com>
2025-11-06 15:59:12 +0545

    Second Class

--- app/src/main/java/com/example/classclasstufftuff/MainActivity.kt
+++ app/src/main/java/com/example/classclasstufftuff/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.classclasstufftuff.ui.theme.ClassclasstufftuffTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +29,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ClassclasstufftuffTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainBody() {
+    Scaffold { padding ->
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues = padding)
+            .background(color = Color.Red),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+
+
+
+
+    ) {
+        Text(text = "Hello", style = TextStyle(
+            fontSize = 67.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            textAlign = TextAlign.Start,
+            fontStyle = FontStyle.Italic,
+            textDecoration = TextDecoration.Underline
+            )
+        )
+        Text(text = "Andriod", style = TextStyle(
+            fontSize = 67.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            textAlign = TextAlign.Start,
+            fontStyle = FontStyle.Italic,
+            textDecoration = TextDecoration.Underline
+        )
+        )
+
+       }
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun GreetingPreview() {
-    ClassclasstufftuffTheme {
-        Greeting("Android")
-    }
+fun MainPreview () {
+    MainBody()
 }
